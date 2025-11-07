c26e7ab1fb195d0ca45a6da99ea14b22cc53d521
KYs <amitkys59@gmail.com>
2025-11-07 21:01:49 +0530

    change tracking file

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
-// @ts-expect-error - PrismaPlugin is not a valid type
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {},
   /* config options here */
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./node_modules/.prisma/client/**/*"],
 
-    return config;
+    "/*": ["./node_modules/.prisma/client/**/*"],
   },
 };
 
