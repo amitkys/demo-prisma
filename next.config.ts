3d9b589ae76f6ae2d571695effd37bb06151470b
KYs <amitkys59@gmail.com>
2025-11-07 20:52:38 +0530

    addded plugin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+// @ts-expect-error - PrismaPlugin is not a valid type
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
